2f4b9133e38255b1f3671b2b686f925175fd674a
svillamizar707 <stevenvillamizar@hotmail.es>
2025-10-16 10:05:26 -0500

    feat: se agregan dtos, services, y AuthController

---
+++ TravelRequests/TravelRequests.Api/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelRequests.Application.DTOs;
+using TravelRequests.Application.Services;
+
+namespace TravelRequests.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterDto dto)
+        {
+            try
+            {
+                var response = await _authService.Register(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+         
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto dto)
+        {
+            try
+            {
+                var response = await _authService.Login(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
+        }
+    }
+}
