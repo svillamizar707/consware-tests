48f7bac04a9ecfa7b436dff65f130af65508929e
svillamizar707 <stevenvillamizar@hotmail.es>
2025-10-15 19:06:04 -0500

    Se agregan clases de Domain, Interfaces y DbContext.

---
+++ TravelRequests/TravelRequests.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelRequests.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "Solicitante"; // 'Solicitante' o 'Aprobador'
+
+        // Relación
+        public ICollection<TravelRequest> TravelRequests { get; set; } = new List<TravelRequest>();
+    }
+}
