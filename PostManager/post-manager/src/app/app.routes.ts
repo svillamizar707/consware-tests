ec68e4e6eed7bb72d5c3fb2e1b379fb1e2f3be30
svillamizar707 <stevenvillamizar@hotmail.es>
2025-10-16 15:10:28 -0500

    feat(post-form): componente reutilizable para crear/editar publicaciones (Reactive Forms)

--- PostManager/post-manager/src/app/app.routes.ts
+++ PostManager/post-manager/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { PostFormComponent } from './posts/post-form/post-form.component';
+
 export const routes: Routes = [
 	{ path: '', redirectTo: 'posts', pathMatch: 'full' },
-	{ path: 'posts', component: PostsListComponent }
+	{ path: 'posts', component: PostsListComponent },
+	{ path: 'posts/new', component: PostFormComponent },
+	{ path: 'posts/:id/edit', component: PostFormComponent }
 ];
