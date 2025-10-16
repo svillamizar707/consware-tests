79f47f2e27aeeb8f71065f6c5b00dfddd140fe40
svillamizar707 <stevenvillamizar@hotmail.es>
2025-10-16 16:23:15 -0500

    feat(routing): lazy-load PostsModule with loadChildren

--- PostManager/post-manager/src/app/app.routes.ts
+++ PostManager/post-manager/src/app/app.routes.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { PostsListComponent } from './posts/posts-list/posts-list.component';
-import { PostFormComponent } from './posts/post-form/post-form.component';
-import { PostDetailComponent } from './posts/post-detail/post-detail.component';
-
 export const routes: Routes = [
 	{ path: '', redirectTo: 'posts', pathMatch: 'full' },
-	{ path: 'posts', component: PostsListComponent },
-	{ path: 'posts/new', component: PostFormComponent },
-	{ path: 'posts/:id/edit', component: PostFormComponent },
-	{ path: 'posts/:id', component: PostDetailComponent }
+	{ path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) }
 ];
